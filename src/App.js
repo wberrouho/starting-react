dc159927cff9ce5ebadf24ba9861e0d2b78820b4
Wassim Berrouho <wberrouho@gmail.com>
2024-09-08 17:56:24 +0200

    Jherr second lesson

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <div >
+    <div className='title'>
+      <h1
+      style={{
+        margin : "auto",
+        witdth : 800,
+        paddingTop : "1rem"
+
+      }}  
+      >Pokemon search</h1>
+      <table width="100%">
+      <thead>
+      <tr>
+          <th>Name</th>
+          <th>Type </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+          <td>bulbassur</td>
+          <td>grass , poison </td>
+      </tr>
+      </tbody>
+      </table>
     </div>
   );
 }
